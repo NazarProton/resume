da2569aa67d8660858c8d44e34d26d6acc7b5383
Nazar Proton <nproton2012@gmail.com>
2024-10-07 19:05:00 +0200

    last updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <main className="flex items-center justify-center min-h-screen w-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Nazar Proton</h1>
+      <h1 className="text-4xl font-bold text-blue-500">
+        Nazar Proton JS Devvvv
+      </h1>
     </main>
   );
 }
